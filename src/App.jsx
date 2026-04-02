7365f5e38b7dc29df224cba41b625376f2a07a3e
Eakkarach <eakoh101@gmail.com>
2026-04-03 00:37:19 +0700

    test:react-state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import "./App.css";
+import React, { useState } from 'react';
+
 
 function App() {
+  const[hello, setHello] = useState("Greeting Message")
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{hello}</div>
+
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={()=>setHello("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={()=>setHello("Hi!")}>Hi!</button>
+        <button onClick={()=>setHello("你好!")}>你好!</button>
       </div>
     </div>
   );
 }
 
 export default App;
+
+
